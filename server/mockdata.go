30670808f013ae3ed331945ceee604528a6c97e9
Ian Duncan <ianduncan08@gmail.com>
2019-05-03 17:37:19 +0300

    chore(setup): setup graphql using gqlgen
    - pull gqlgen for initiating our project

---
+++ server/mockdata.go
@@ -0,0 +1,12 @@
+package server
+
+import "github.com/Duncanian/iam-gateway/server/models"
+
+var IamUsers = models.Users{
+	models.User{
+		ID:      "id",
+		Name:    "name",
+		Email:   "email",
+		Picture: "picture",
+	},
+}
